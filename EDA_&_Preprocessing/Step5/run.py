1c7225e7897b043a302a545dbf938965724fb063
Adam Elshimi <adamelshimi@Adams-Air.fritz.box>
2023-02-24 05:16:33 +0100

    ml pipeline and eda

---
+++ EDA_&_Preprocessing/Step5/run.py
@@ -0,0 +1,73 @@
+
+import argparse
+import logging
+import pandas as pd
+import wandb
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    run = wandb.init(project="exercise_5", job_type="process_data")
+    logger.info('downloading artifact')
+    ## YOUR CODE HERE
+    data = run.use_artifact(args.input_artifact)
+    df = pd.read_parquet(artifact_path)
+
+    logger.info('dropping duplicates')
+    df = df.drop_duplicates().reset_index(frop=True)
+    
+    logger.info('fixing missing values')
+    df['title'].fillna(value='', inplace=True)
+    
+    df['song_name'].fillna(value='', inplace=True)
+    df['text_feature'] = df['title'] + ' ' + df['song_name']
+   
+    filename = 'preprocessed_data.csv' 
+    df.to_csv(filename)
+    artifact = wandb.Artifact(
+        name: args.artifact_name,
+        type: args.artifact_type,
+        description: args.artifact_description
+    )
+        
+   artifact.add_file(filename)
+
+   logger.info('logging artifact')
+   run.log_artifact(artifact) 
+   
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Preprocess a dataset",
+        fromfile_prefix_chars="@",
+    )
+
+    parser.add_argument(
+        "--input_artifact",
+        type=str,
+        help="Fully-qualified name for the input artifact",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--artifact_name", type=str, help="Name for the artifact", required=True
+    )
+
+    parser.add_argument(
+        "--artifact_type", type=str, help="Type for the artifact", required=True
+    )
+
+    parser.add_argument(
+        "--artifact_description",
+        type=str,
+        help="Description for the artifact",
+        required=True,
+    )
+
+    args = parser.parse_args()
+
+    go(args)
