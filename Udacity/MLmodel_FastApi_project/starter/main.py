4484676a1effc8cf96c337f2c05bdeaf0ab737e8
Adam Elshimi <adamelshimi@Adams-Air.fritz.box>
2023-03-23 13:58:55 +0100

    updated train_model file

--- Udacity/MLmodel_FastApi_project/starter/main.py
+++ Udacity/MLmodel_FastApi_project/starter/main.py
@@ -6,29 +6,5 @@ Created on Thu Mar 23 11:59:31 2023
 @author: adamelshimi
 """
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
-
-filename = "./adult.data"
-
-cols = ['age', 'workclass','fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 
-        'capital-gain', 'capital-loss', 'hours-per-week', 'native-country','salary']
-
-data = pd.read_csv(filename, names=cols)
-
-df = pd.DataFrame(data)
-
-Y = df.pop('salary') 
-X = df
-
-
-# X_train
-# x_val
-# Y_train
-# y_val
-
-
-if __name__=="__main__":
-    print(data.head())
-    #print(data.describe())
-    print(Y)
-    print(X) 
